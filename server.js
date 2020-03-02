822aea27c06219c1d308c7362500b08bb7ca0581
Mardonio Silva da Costa <mrdonio.sc@gmail.com>
2020-03-02 10:07:05 -0300

    :rocket: Meu primeiro deploy no Heroku

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const app = express();
 
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+app.get('*', (req, res) => {
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
 });
 
 app.listen(process.env.PORT || 8080);
